77112d27c826b07f45d0c5e9a4b3df841dca2229
Willy7797 <William.priestly2@education.nsw.gov.au>
2024-08-22 12:35:39 +1000

    start - imports, classes and definitions
    I have added the classess we need, and definitions. as well as the imports.

---
+++ main.py
@@ -0,0 +1,33 @@
+from machine import Pin, PWM
+import utime
+import random
+
+class SimonGame:
+    def __init__(self):
+        pass
+
+    def play_tone(self):
+        pass
+
+    def flash_led(self):
+        pass
+
+    def play_sequence(self):
+        pass
+
+    def get_player_input(self):
+        pass
+
+    def start_game(self):
+        pass
+
+    def stop_game(self):
+        pass
+
+    def run(self):
+        pass
+    def cleanup(self):
+        pass
+
+game = SimonGame()
+game.run()
