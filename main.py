8d520ecb9c6b9ae8bc4fc50f2801e02a7fa8001a
Willy7797 <William.priestly2@education.nsw.gov.au>
2024-08-22 12:38:35 +1000

    2nd - pins and necessity
    'def __init__' was set up, including game necessities and pin locations

--- main.py
+++ main.py
@@ -4,7 +4,21 @@ import random
 
 class SimonGame:
     def __init__(self):
-        pass
+        #Initialize GPIO pins
+        self.led_pins = [Pin(28, Pin.OUT), Pin(27, Pin.OUT), Pin(26, Pin.OUT), Pin(22, Pin.OUT)]
+        self.button_pins = [Pin(2, Pin.IN, Pin.PULL_DOWN), Pin(3, Pin.IN, Pin.PULL_DOWN), Pin(4, Pin.IN, Pin.PULL_DOWN), Pin(5, Pin.IN, Pin.PULL_DOWN)]
+        self.speaker_pins = [PWM(Pin(18)), PWM(Pin(19)), PWM(Pin(20)), PWM(Pin(21))]
+        self.start_switch = Pin(14, Pin.IN, Pin.PULL_DOWN)
+        self.stop_switch = Pin(15, Pin.IN, Pin.PULL_DOWN)
+
+        #Frequencies for the speakers corresponding to each LED
+        self.tones = [440, 550, 660, 770]
+
+
+        #Game state
+        self.sequence = []
+        self.player_sequence = []
+        self.score = 0
 
     def play_tone(self):
         pass
